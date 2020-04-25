774cc70a1541d1506ed545f14ce46af9715b0b75
Radosław Kuczyński <krooopel@gmail.com>
2020-04-25 22:16:45 +0200

    Updates

---
+++ precache-manifest.adfe8980b6cc0528ca1f0d568db34ecf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ae2f7c866e952cccdb2aa259adf5c29",
+    "url": "/chess/index.html"
+  },
+  {
+    "revision": "a55412a48fc155a41aa0",
+    "url": "/chess/static/css/main.f5ebf681.chunk.css"
+  },
+  {
+    "revision": "06235e96d49edee55838",
+    "url": "/chess/static/js/2.43a0af64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chess/static/js/2.43a0af64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a55412a48fc155a41aa0",
+    "url": "/chess/static/js/main.9a8964e2.chunk.js"
+  },
+  {
+    "revision": "25b9da6d7133151397dd",
+    "url": "/chess/static/js/runtime-main.4cacbaa4.js"
+  }
+]);
